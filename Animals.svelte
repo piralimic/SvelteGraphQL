0fd3879250253c26c55614cdb5fc8344914c1877
Michaël Pirali <54063192+piralimic@users.noreply.github.com>
2020-09-04 21:14:03 +0200

    Initial commit

---
+++ Animals.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { getClient, query } from "svelte-apollo";
+  import { gql } from "apollo-boost";
+
+  const client = getClient();
+  const GET_ANIMALS = gql`
+    {
+      animal {
+        id
+        name
+        type {
+          value
+        }
+        sex {
+          value
+        }
+        birthdate
+        description
+        picture_url
+        reference_number
+        sponsorings {
+          sponsor {
+            first_name
+            last_name
+          }
+        }
+      }
+    }
+  `;
+  const animals = query(client, { query: GET_ANIMALS });
+
+  function hasSponsor(sponsorings) {
+    return sponsorings.length > 0 ? true : false;
+  }
+</script>
+
+<h2>Animals</h2>
+{#await $animals}
+  Loading...
+{:then result}
+  <p>Total: {result.data.animal.length}</p>
+  <hr>
+  {#each result.data.animal as animal}
+    Référence {animal.reference_number}
+    <h3>{animal.name}</h3>
+    <p>{animal.type.value}</p>
+    <p>{animal.sex.value}</p>
+    <p>{animal.birthdate}</p>
+    <img alt="picture of {animal.name}" src="{animal.picture_url}" width="200px">
+    <p>{animal.description}</p>
+    {#if hasSponsor(animal.sponsorings)}
+      {animal.name} est parrainé par 
+      {#each animal.sponsorings as sponsoring}
+        {sponsoring.sponsor.first_name} {sponsoring.sponsor.last_name}
+      {/each}
+    {/if}
+    <hr>
+  {/each}
+{:catch error}
+  {error}
+{/await}
---
+++ App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Animal from "./Animals.svelte";
+
+  import ApolloClient from "apollo-boost";
+  import { setClient } from "svelte-apollo";
+
+  const client = new ApolloClient({
+    uri: "https://known-rattler-86.hasura.app/v1/graphql"
+  });
+
+  setClient(client);
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Svelte GraphQL</h1>
+  <Animal />
+</main>
